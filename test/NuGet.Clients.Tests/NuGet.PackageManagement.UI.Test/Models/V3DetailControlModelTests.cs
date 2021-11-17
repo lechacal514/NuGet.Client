bceea0ebae554cc8427699fec0f8e6b9c6bc1f9c
Martin Ruiz <martin.ruiz.mares@gmail.com>
2021-11-17 14:40:22 -0800

    pr comments

--- test/NuGet.Clients.Tests/NuGet.PackageManagement.UI.Test/Models/V3DetailControlModelTests.cs
+++ test/NuGet.Clients.Tests/NuGet.PackageManagement.UI.Test/Models/V3DetailControlModelTests.cs
@@ -201,11 +201,11 @@ namespace NuGet.PackageManagement.UI.Test.Models
         }
 
         [Theory]
-        [InlineData(NuGetProjectKind.PackageReference, ProjectModel.ProjectStyle.PackageReference, "*", "2.10.0")]
-        [InlineData(NuGetProjectKind.PackageReference, ProjectModel.ProjectStyle.PackageReference, "1.*", "2.10.0")]
-        [InlineData(NuGetProjectKind.PackageReference, ProjectModel.ProjectStyle.PackageReference, "(0.1,3.4)", "2.10.0")]
-        [InlineData(NuGetProjectKind.PackageReference, ProjectModel.ProjectStyle.PackageReference, "2.10.0", "2.10.0")]
-        public async void IsSelectedVersionCorrect_WhenPackageStyleIsPackageReference_And_CustomVersion(NuGetProjectKind projectKind, ProjectModel.ProjectStyle projectStyle, string allowedVersions, string installedVersion)
+        [InlineData(NuGetProjectKind.PackageReference, "*", "2.10.0")]
+        [InlineData(NuGetProjectKind.PackageReference, "1.*", "2.10.0")]
+        [InlineData(NuGetProjectKind.PackageReference, "(0.1,3.4)", "2.10.0")]
+        [InlineData(NuGetProjectKind.PackageReference, "2.10.0", "2.10.0")]
+        public async void IsSelectedVersionCorrect_WhenPackageStyleIsPackageReference_And_CustomVersion(NuGetProjectKind projectKind, string allowedVersions, string installedVersion)
         {
             // Arange project
             var mockServiceBroker = new Mock<IServiceBroker>();
@@ -239,7 +239,7 @@ namespace NuGet.PackageManagement.UI.Test.Models
             var project = new Mock<IProjectContextInfo>();
 
             project.SetupGet(p => p.ProjectKind).Returns(projectKind);
-            project.SetupGet(p => p.ProjectStyle).Returns(projectStyle);
+            project.SetupGet(p => p.ProjectStyle).Returns(ProjectModel.ProjectStyle.PackageReference);
             project.SetupGet(p => p.ProjectId).Returns("ProjectId");
 
             var model = new PackageDetailControlModel(
@@ -247,7 +247,7 @@ namespace NuGet.PackageManagement.UI.Test.Models
                 solutionManager: new Mock<INuGetSolutionManagerService>().Object,
                 projects: new[] { project.Object });
 
-            // Arrange Package
+            // Arrange
             var testVersions = new List<VersionInfoContextInfo>() {
                 new VersionInfoContextInfo(new NuGetVersion("2.10.1-dev-01248")),
                 new VersionInfoContextInfo(new NuGetVersion("2.10.0")),
@@ -256,6 +256,10 @@ namespace NuGet.PackageManagement.UI.Test.Models
             var searchService = new Mock<IReconnectingNuGetSearchService>();
             searchService.Setup(ss => ss.GetPackageVersionsAsync(It.IsAny<PackageIdentity>(), It.IsAny<IReadOnlyCollection<PackageSourceContextInfo>>(), It.IsAny<bool>(), It.IsAny<CancellationToken>()))
                 .ReturnsAsync(testVersions);
+
+            VersionRange installedVersionRange = VersionRange.Parse(allowedVersions, true);
+            NuGetVersion bestVersion = installedVersionRange.FindBestMatch(testVersions.Select(t => t.Version));
+            var displayVersion = new DisplayVersion(installedVersionRange, bestVersion, additionalInfo: null);
 
             // Act
             var vm = new PackageItemViewModel(searchService.Object)
@@ -273,12 +277,9 @@ namespace NuGet.PackageManagement.UI.Test.Models
                 () => vm);
 
             // Assert
-            VersionRange installedVersionRange = VersionRange.Parse(allowedVersions, true);
-            NuGetVersion bestVersion = installedVersionRange.FindBestMatch(testVersions.Select(t => t.Version));
-            var displayVerion = new DisplayVersion(installedVersionRange, bestVersion, additionalInfo: null);
 
             Assert.Equal(model.SelectedVersion.ToString(), allowedVersions);
-            Assert.Equal(model.Versions.FirstOrDefault(), displayVerion);
+            Assert.Equal(model.Versions.FirstOrDefault(), displayVersion);
         }
 
         public Task<object> GetServiceAsync(Type serviceType)
